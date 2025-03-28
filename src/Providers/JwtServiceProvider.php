64c98fb9853cb1fe15797263fb6d812b53f59abb
taha jaiti <kyojin.rar@gmail.com>
2025-03-28 01:05:28 +0000

    FEAT: added the documentation to the JwtService provider

--- src/Providers/JwtServiceProvider.php
+++ src/Providers/JwtServiceProvider.php
@@ -6,25 +6,52 @@ use Kyojin\JWT\Services\JwtService;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * JwtServiceProvider for Laravel JWT Authentication Package
+ * 
+ * This service provider handles the configuration, binding, and middleware 
+ * registration for the JWT authentication package.
+ * 
+ * @package Kyojin\JWT\Providers
+ * @version 1.0.1
+ */
 class JwtServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap any application services.
+     * 
+     * This method is responsible for:
+     * - Publishing configuration files
+     * - Registering package middleware
+     * 
+     * @return void
+     */
     public function boot()
     {
-        // Publishing the config
+        // publish config file to allow customization
         $this->publishes([
             __DIR__ . '/../../config/jwt.php' => $this->app->configPath('jwt.php'),
         ], 'jwt-config');
 
-        // Registering the middleware
+        // register the jwt middleware
         Route::aliasMiddleware('jwt', \Kyojin\JWT\Http\Middleware\JwtMiddleware::class);
     }
 
+    /**
+     * Register any application services.
+     * 
+     * This method is responsible for:
+     * - Merging package configuration
+     * - Binding the JWT service to the service container
+     * 
+     * @return void
+     */
     public function register()
     {
-        // Merge the config
+        // using laravel pre-defined method to merge config files
         $this->mergeConfigFrom(__DIR__ . '/../../config/jwt.php', 'jwt');
 
-        // Binding to the service container
+        // bind the jwt service to the service container
         $this->app->singleton('JWT', function () {
             return new JwtService();
         });
